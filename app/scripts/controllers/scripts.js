496084664c9b6ffc7b86102e875cf8fdf37d96f6
fangyuwei <245310999@qq.com>
2015-09-13 10:39:06 +0800

    begin

---
+++ app/scripts/controllers/scripts.js
@@ -0,0 +1,52 @@
+
+var app = angular.module('myApp', []);
+app.controller('InvoiceCtrl', function ($scope) {
+    $scope.qty=1;
+    $scope.cost=9.5;
+});
+//指令
+app.directive('draggable', function ($document) {
+
+    var startX= 0,startY= 0,x= 0,y=0;
+
+    return function (scope,element,attr) {
+        element.css({
+            position:'relative',
+            border:'1px solid red',
+            background:'lighter'
+        });
+
+    }
+
+})
+/*app.directive('draggable', function ($document) {
+    var startX = 0, startY = 0, x = 0, y = 0;
+    return function (scope, element, attr) {
+        element.css({
+            position: 'relative',
+            border: '1px solid red',
+            backgroundColor: 'lightgrey',
+            cursor: 'pointer'
+        });
+        element.bind('mousedown', function (event) {
+            startX = event.screenX - x;
+            startY = event.screenY - y;
+            $document.bind('mousemove', mousemove);
+            $document.bind('mouseup', mouseup);
+        });
+
+        function mousemove(event) {
+            y = event.screenY - startY;
+            x = event.screenX - startX;
+            element.css({
+                top: y + 'px',
+                left: x + 'px'
+            });
+        }
+
+        function mouseup() {
+            $document.unbind('mousemove', mousemove);
+            $document.unbind('mouseup', mouseup);
+        }
+    }
+});*/
